49f9a680002c8bc2f49da97eb4cbfef4c2d34559
daniel-toth-everit <daniel.toth@everit.biz>
2022-04-19 20:20:50 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Prioritization Custom Field Forge 
+
+This simple Forge app adds a prioritization custom field that you can configure on your issue screens in classic projects.
+The purpose of the app is to demonstrate how to develope quickly serverless applications with Atlassian Forge.
+
+## Requirements
+
+See [Set up Forge](https://developer.atlassian.com/platform/forge/set-up-forge/) for instructions to get set up.
+
+## Quick start
+
+Once you have logged into the CLI (`forge login`), follow the steps below to install the app onto your site:
+
+1. Clone this repository
+2. Run `forge register` to register a new copy of this app to your developer account
+3. Run `npm install` to install your dependencies
+4. Run `forge deploy` to deploy the app into the default environment
+5. Run `forge install` and follow the prompts to install the app
+
+## Usage
+
+* Go the the list of custom fields.
+* Find the *Risk assessment* custom field.
+* Click the *more* menu, and then select *Associate to Screens*.
+* Check the screens you want to use the field on, and then click *Update*.
+
+## Documentation
+
+The app consists of one custom field defined in the `manifest.yml` file. 
+The custom field's view is implemented by the `renderFieldView` function defined in `/src/index.jsx`. 
+The custom field�s editing experience is implemented by the `renderFieldEdit` function defined in `/src/index.jsx`. 
+
+## Contributions
+
+Contributions to Forge Risk Assessment Custom Field are welcome! Please see [CONTRIBUTING.md](./CONTRIBUTING.md) for details. 
+
+## License
+
+Copyright (c) 2020 Atlassian and others.
+Apache 2.0 licensed, see [LICENSE](./LICENSE) file.
---
+++ src/index.jsx
@@ -0,0 +1,70 @@
+import ForgeUI, { CustomField, CustomFieldEdit, StatusLozenge, render, Text, useProductContext, Fragment, Select, Option } from "@forge/ui";
+
+const View = () => {
+  const getLozengeApperance = (priorityLevel) => {
+      switch (priorityLevel) {
+          case 'low':
+              return 'success';
+          case 'medium':
+              return 'inprogress';
+          case 'high':
+              return 'removed';
+          default:
+              return 'default'
+      }
+  }
+
+  const {
+      extensionContext: { fieldValue },
+  } = useProductContext();
+
+  return (
+      <CustomField>
+          <Text>
+              <StatusLozenge text={fieldValue || 'None'} appearance={getLozengeApperance(fieldValue)} />
+          </Text>
+      </CustomField>
+  );
+};
+
+const Edit = () => {
+  const onSubmit = (formValue) => {
+      const demand = +formValue.demand;
+      const impact = +formValue.impact;
+      const effort = +formValue.effort;
+      const priorityValue = demand + impact + effort;
+
+      if (priorityValue <= 4) {
+          return 'high';
+      } else if (priorityValue <= 6) {
+          return 'medium';
+      } else {
+          return 'low'
+      }
+  }
+
+  return (
+      <CustomFieldEdit onSubmit={onSubmit} header="Select risk" width="medium" >
+          <Fragment>
+              <Select label="Demand" name="demand" isRequired>
+                  <Option label="Low" value="3" />
+                  <Option label="Medium" value="2" />
+                  <Option label="High" value="1" />
+              </Select>
+              <Select label="Impact" name="impact" isRequired>
+                  <Option label="Low" value="1" />
+                  <Option label="Medium" value="2" />
+                  <Option label="High" value="3" />
+              </Select>
+              <Select label="Effort" name="effort" isRequired>
+                  <Option label="Low" value="1" />
+                  <Option label="Medium" value="2" />
+                  <Option label="High" value="3" />
+              </Select>
+          </Fragment>
+      </CustomFieldEdit>
+  );
+};
+
+export const runView = render(<View/>);
+export const runEdit = render(<Edit/>)
